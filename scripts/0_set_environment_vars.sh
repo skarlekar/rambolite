51cc22b8337d736e9880c25e2ea8d8617d89226c
Srini Karlekar <skarlekar@yahoo.com>
2019-10-04 08:53:11 -0400

    Added comments to the set_environment scripts

--- scripts/0_set_environment_vars.sh
+++ scripts/0_set_environment_vars.sh
@@ -1,20 +1,48 @@
 export SCRIPTS_DIR=./scripts
+
+# Path to your EC2 key.pem file
 export SSH_ID_FILE=~/k8s.pem
 
-export CONTROLLER0_HOST=ec2-34-197-68-244.compute-1.amazonaws.com
-export CONTROLLER0_PUBLIC_IP=34.197.68.244
+# Host name of your controller 1
+export CONTROLLER0_HOST=
+
+# Public IP of your Controller 1
+export CONTROLLER0_PUBLIC_IP=
+
+# Private IP of your Controller 1
 export CONTROLLER0_IP=192.168.0.17
-export CONTROLLER1_HOST=ec2-34-204-114-233.compute-1.amazonaws.com
-export CONTROLLER1_PUBLIC_IP=34.204.114.233
-export CONTROLLER1_IP=192.168.0.112
-export WORKER0_HOST=ec2-52-0-205-152.compute-1.amazonaws.com
-export WORKER0_IP=192.168.0.65
-export WORKER1_HOST=ec2-52-206-81-218.compute-1.amazonaws.com
-export WORKER1_IP=192.168.0.109
-export LB_HOST=ec2-35-173-106-143.compute-1.amazonaws.com
-export LB_IP=192.168.0.58
+
+# Host name of your controller 2
+export CONTROLLER1_HOST=
+
+# Public IP of your Controller 2
+export CONTROLLER1_PUBLIC_IP=
+
+# Private IP of your Controller 2
+export CONTROLLER1_IP=
+
+# Host name of your worker 1 node
+export WORKER0_HOST=
+
+# Private IP of your worker 1 node
+export WORKER0_IP=
+
+# Host name of your worker 2 node
+export WORKER1_HOST=
+
+# Private IP of your worker 2 node
+export WORKER1_IP=
+
+# Host name of your load balancer
+export LB_HOST=
+
+# Private IP of your load balancer
+export LB_IP=
+
+# Leave this as it is
 export CERT_HOSTNAME=10.32.0.1,$CONTROLLER0_IP,$CONTROLLER0_HOST,$CONTROLLER1_IP,$CONTROLLER1_HOST,$LB_IP,$LB_HOST,127.0.0.1,localhost,kubernetes.default
 export INITIAL_CLUSTER=$CONTROLLER0_HOST=https://$CONTROLLER0_IP:2380,$CONTROLLER1_HOST=https://$CONTROLLER1_IP:2380
 
+# Leave this as it is
 # Set KUBERNETES_ADDRESS=<load balancer private ip>
 export KUBERNETES_ADDRESS=$LB_IP
